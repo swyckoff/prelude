2ae6e0c915efb84ca04fb67c737fd3fa22438524
Steve Wyckoff <sympl@Steves-MacBook-Pro.local>
2013-12-07 02:59:08 -0500

    Added personal settings

---
+++ personal/mac.el
@@ -0,0 +1,30 @@
+;; Mac specific settings
+
+;; MAC SPECIFIC
+;; BUG - Read in PATH from .zprofile
+;; (if (not (getenv "TERM_PROGRAM"))
+;;     (setenv "PATH"
+;;         (shell-command-to-string "source $HOME/.zprofile && printf $PATH")))
+
+(when (memq window-system '(mac ns)) (exec-path-from-shell-initialize)) ;;Launching emacs from gui
+        
+(setq mac-command-modifier 'meta)
+;; (setq mac-option-modifier 'alt)
+(setq mac-option-modifier 'super)
+
+;; Allow tabbing between frames
+(x-focus-frame nil)
+(global-set-key "\M-`" 'other-frame)
+
+
+;(defun copy-from-osx ()
+;  (shell-command-to-string "pbpaste"))
+
+;(defun paste-to-osx (text &optional push)
+;  (let ((process-connection-type nil))
+;    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;      (process-send-string proc text)
+;      (process-send-eof proc))))
+
+;(setq interprogram-cut-function 'paste-to-osx)
+;(setq interprogram-paste-function 'copy-from-osx)
---
+++ personal/my.el
@@ -0,0 +1,87 @@
+;; Personal emacs customizations
+;;
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(when (not package-archive-contents)
+
+  (package-refresh-contents))
+
+
+(defvar my-packages '(color-theme-solarized auto-complete ac-nrepl popup undo-tree));   ;auto-complete ac-nrepl nrepl
+
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Pasting
+(setq x-select-enable-clipboard t)
+(setq-default inhibit-startup-screen t)
+
+;; Colors
+;;(disable-theme 'zenburn)
+(load-theme 'solarized-dark t)
+
+;; Always show column numbers.
+(setq-default column-number-mode t)
+
+;; Sunrise Commander
+;(require 'sunrise-commander)
+(global-set-key (kbd "C-c x") 'sunrise)
+(global-set-key (kbd "C-c X") 'sunrise-cd)
+
+;; For easy window scrolling up and down.
+(global-set-key "\M-n" 'scroll-up-line)
+(global-set-key "\M-p" 'scroll-down-line)
+
+;; For easier access to regex search/replace.
+(defalias 'qrr 'query-replace-regexp)
+
+;; Some default eldoc facilities
+(add-hook 'nrepl-connected-hook
+(defun pnh-clojure-mode-eldoc-hook ()
+(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(nrepl-enable-on-existing-clojure-buffers)))
+
+
+;; Clojure speific emacs
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(setq nrepl-hide-special-buffers t) ; hides *server..repl stufff
+
+;;Cider settings
+
+(setq cider-repl-pop-to-buffer-on-connect nil)
+(setq cider-popup-stacktraces nil)
+(setq cider-repl-popup-stacktraces t)
+(setq cider-auto-select-error-buffer t)
+(setq nrepl-buffer-name-show-port t)
+(setq cider-repl-display-in-current-window t)
+
+(setq cider-repl-wrap-history t)
+(setq cider-repl-history-size 1000) ; the default is 500
+(setq cider-repl-history-file "~/.emacs.d/nrepl-history")
+
+(add-hook 'cider-repl-mode-hook 'subword-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+;; Auto Complete
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'cider-repl-mode))
+
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(eval-after-load "cider"
+  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
